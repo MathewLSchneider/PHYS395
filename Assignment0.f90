6295e8281811c75bc93e5950b49d7ab5f76118e1
PHYS395 - Computational Physics Sandbox <phys395@sandbox.phys.sfu.ca>
2019-01-13 22:12:12 -0800

    Assignment 0

---
+++ Assignment0.f90
@@ -0,0 +1,27 @@
+! compile with: gfortran Assignment0.f90 -o Fibonacci
+! run with:     ./Fibonacci > DATA
+
+
+program Fibonacci
+implicit none
+
+integer*8 a, b, c, i !make the variables 8-bit integers as lots of digits are needed
+integer, parameter :: n = 50 !counting parameter
+
+!Initialize first two values of Fibonacci
+a = 0
+b = 1
+
+write (*,*) a
+write (*,*) b !Print the two first numbers
+
+do i = 1,n
+
+  c = a + b !!Generate the next Fibonacci number
+  a = b     !!Shift over the variable a and b
+  b = c
+  write (*,*) c  !Print only the next number
+
+end do
+
+end
