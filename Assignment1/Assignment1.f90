540a8222099698afd914fae16c90fac474a1c2cf
PHYS395 - Computational Physics Sandbox <phys395@sandbox.phys.sfu.ca>
2019-01-22 16:31:39 -0800

    A1 start

---
+++ Assignment1/Assignment1.f90
@@ -0,0 +1,70 @@
+! gaussj.f90  -  starting point for Gauss-Jordan elimination
+! compile with: gfortran -O3 -fdefault-real-8 gaussj.f90
+
+program test
+implicit none
+
+real A(3,3), B(3)
+
+! matrix to invert
+A(1,:) = [1.0, 2.0, 3.0]
+A(2,:) = [3.0, 2.0, 1.0]
+A(3,:) = [0.0, 1.0, 0.0]
+
+B = [1.0, 2.0, 3.0]
+
+write (*,*) A
+
+write (*,*) maxloc(A(2:,2:))
+
+ call gaussj(3, A, B)
+
+!write (*,*) B
+
+contains
+
+! solve A.x = B using Gauss-Jordan elimination
+! A gets destroyed, answer is returned in B
+subroutine gaussj(n, A, B)
+	integer n, i, j; real A(n,n), B(n)
+	
+	! pivot indices
+	integer row(n), col(n), pivot(2)
+	
+	forall (i=1:n) row(i) = i
+	forall (j=1:n) col(j) = j
+	
+
+
+	! access data like A(row(i),col(j))
+	
+	! find location of maximal value
+	pivot = maxloc(abs(A))
+
+	! swap rows/columns
+	row([1,pivot(1)]) = row([pivot(1),1])
+	col([1,pivot(2)]) = col([pivot(2),1])
+
+	!A([1,pivot(1)],:) = A([pivot(1),1],:)
+	!A(:,[1,pivot(2)]) = A(:,[pivot(2),1])
+	!B([1,pivot(1)]) = B([pivot(1),1])
+
+	!write (*,*) A
+	!write (*,*) B
+
+	! get rid of first column
+	do i = 2,n
+		A(i,:) = A(i,:) - (A(i,1)/A(1,1)) * A(1,:)
+		B(i) = B(i) - (A(i,1)/A(1,1)) * B(1)
+	end do
+
+	write (*,*) A
+	write (*,*) B
+	
+	! if you want to iterate by recursion
+	!call gaussj(n-1, A(2:,2:), B(2:))
+	
+	! otherwise, do loop on k
+end subroutine
+
+end
